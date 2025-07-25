9854b271c8974c7c08fde7d2556c85cf2578debb
mackliu <macklun@gmail.com>
2025-07-25 13:21:15 +0800

    更新院線版資料表

--- db02.sql
+++ db02.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-07-25 07:19:03
+-- 產生時間： 2025-07-25 07:20:47
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -37,6 +37,7 @@ CREATE TABLE `movies` (
   `director` text NOT NULL,
   `trailler` text NOT NULL,
   `poster` text NOT NULL,
+  `intro` text NOT NULL,
   `sh` int(1) NOT NULL,
   `rank` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
