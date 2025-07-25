d66c34ad6cc8bb0d8d9d551916c6539bb3ba1370
mackliu <macklun@gmail.com>
2025-07-25 10:37:49 +0800

    顯示下方海報按鈕區

--- front/main.php
+++ front/main.php
@@ -10,6 +10,9 @@
 .btns {
     width: 320px;
     height: 120px;
+    overflow: hidden;
+    display: flex;
+    
 
 }
 .left, .right {
@@ -46,6 +49,19 @@
     height:220px;
     
 }
+.poster-btn{
+    width:80px;
+    height:100px;
+    display: inline-block;
+    flex-shrink: 0;
+    font-size:12px;
+
+}
+.poster-btn img{
+    width:70px;
+    height:90px;
+
+}
     </style>
     <?php 
     $posters=$Poster->all(['sh'=>1]," order by `rank`");
@@ -70,7 +86,19 @@
 
                 <div class="controls">
                     <div class="left"></div>
-                    <div class="btns"></div>
+                    <div class="btns">
+                        <?php 
+                        foreach($posters as $key => $poster):
+                        ?>
+                        <div class='poster-btn ct' data-ani='<?=$poster['ani'];?>' data-id='<?=$poster['id'];?>'>
+                            <img src="./image/<?=$poster['img'];?>" >
+                            <div><?=$poster['name'];?></div>
+                        </div>
+
+                        <?php
+                        endforeach;
+                        ?>
+                    </div>
                     <div class="right"></div>
                 </div>
             </div>
@@ -107,18 +135,16 @@ console.log(ani);
         //淡入淡出
         $(now).fadeOut(1000);
         $(next).fadeIn(1000);
-
-
     break;
     case 2:
-        $(now).fadeOut(1000);
-        $(next).fadeIn(1000);
-
+        //縮放
+        $(now).hide(1000);
+        $(next).show(1000);
     break;
     case 3:
-        $(now).fadeOut(1000);
-        $(next).fadeIn(1000);
-
+        //滑入滑出
+        $(now).slideUp(1000);
+        $(next).slideDown(1000);
     break;
  }
 }
